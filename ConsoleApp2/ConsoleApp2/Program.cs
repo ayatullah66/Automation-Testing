26672a760f3eb76b2335bf3f3b44af0800fe3382
ayatullah66 <ayatullahh04@gmail.com>
2020-12-27 22:14:54 +0200

    FindElements

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -19,9 +19,15 @@ namespace ConsoleApp2
             IWebDriver Driver = new FirefoxDriver(@"F:\Selenium needs");
             
             Driver.Url = "http://www.google.com";
-            
-            Driver.FindElement(By.XPath("//a[contains(text(),'Gmail')]")).Click();
-            
+            Driver.FindElement(By.Name("q")).SendKeys("ID");
+            Driver.FindElement(By.ClassName("gLFyf")).SendKeys("ClassName");
+            Driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            WebDriverWait Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));    // explicit wait for only needed load process
+            Wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("LC20lb")));
+
+            Driver.FindElements(By.ClassName("LC20lb"))[0].Click();
+
+
 
 
 
