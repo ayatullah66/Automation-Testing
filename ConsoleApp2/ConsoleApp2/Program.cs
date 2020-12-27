5e2b1562800e2f13894eb728a282306b94d0b52b
ayatullah66 <ayatullahh04@gmail.com>
2020-12-27 20:38:34 +0200

    Open Gmail

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            IWebDriver Driver = new FirefoxDriver(@"F:\Selenium needs");
+            
+            Driver.Url = "http://www.google.com";
+            
+            Driver.FindElement(By.XPath("//a[contains(text(),'Gmail')]")).Click();
+            
+
+
+
+
+
+
+        }
+    }
+}
